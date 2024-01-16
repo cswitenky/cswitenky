c2ad931b23643b2dc5dfdbeddcaea73f81faf663
Connor Switenky <cswitenky@tesseract.personal>
2024-01-16 00:01:42 -0800

    expanded on credit card referall links and tweaked all posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -7,7 +7,7 @@ import Date from "../components/date";
 export default function Posts({ allPostsData }: any) {
   return (
     <Layout>
-      <h1>Posts</h1>
+      <h1>All Posts</h1>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         {allPostsData.map(({ id, date, title }: any) => (
           <li key={id}>
