a4fae09dbc959e15ae2e43fbb42f24eb3df86ee4
Connor Switenky <cswitenky@tesseract.personal>
2024-01-20 00:39:37 -0800

    added a 404 image

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,34 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-import { getSortedPostsData } from "../lib/posts";
+import Image from "next/image";
+import utilStyles from "../styles/utils.module.css";
 
 export default function NotFound() {
   return (
-    <Layout home>
+    <Layout>
       <Head>
         <title>Oops!</title>
       </Head>
       <main>
-        <h1>Oops!</h1>
-        <h2>404 - Page Not Found</h2>
-        <p>
-          <a href="/">Go Home</a>
-        </p>
+        <section style={{ textAlign: "center" }}>
+          <Image
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            priority
+            src="/404.png"
+            height={225}
+            width={400}
+            alt={"404"}
+          ></Image>
+          <h1>Page Not Found</h1>
+          <p>
+            <a href="/">Go Home</a>
+          </p>
+        </section>
       </main>
     </Layout>
   );
 }
-
