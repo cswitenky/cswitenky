3a04cfea0dc5e44c761e0d94b591862faad94641
cswitenky <github@mail.switenky.com>
2024-01-23 19:53:42 -0800

    didn't work; configured next-sitemap to output to the 'out' folder

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://switenky.com',
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true,
+  outDir: './out',
   // ...other options
 };
